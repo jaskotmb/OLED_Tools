33cfc2a977bcb838da348784deb4beeb45e0482d
Matt Jaskot <jaskotmb@gmail.com>
2018-09-06 17:30:45 -0600

    180906 working version with trigger

--- OLEDTools.py
+++ OLEDTools.py
@@ -6,78 +6,108 @@ import time
 import csv
 import os
 import visa
+import datetime
 
-def currDecay(sourceCurrent, nplcTime):
+def currDecay(sourceCurrent,maxTime):
     rm = visa.ResourceManager()
-    B2901A_address = rm.list_resources()[0]
-    MM_34460_address = rm.list_resources()[1]
-    smu = rm.open_resource(B2901A_address)
-    mm = rm.open_resource(MM_34460_address)
-    smu.write('*RST')
-    mm.write('*RST')
-    smu.write(':SOUR:FUNC:MODE CURR:DC')
-    smu.write(':SOUR:CURR {}'.format(sourceCurrent))
-    smu.write(':SENS:FUNC ""VOLT""')
-    smu.write(':SENS:VOLT:RANG:AUTO ON')
-    smu.write(':FORM:DATA ASC')
-    smu.write(':OUTP ON')
-    mm.write(':CONF:CURR:DC')
-    mm.write(':CURR:DC:NPLC {}'.format(nplcTime))
-    mm.timeout = 300e3
-    mm.write(':SAMP:COUN 200')
-    brightList = mm.query(':READ?').split('\n')[0]
-    brightList = list(zip(brightList,brightList,brightList))
-    return brightList
-
-def IVBSweep(Vbegin, Vend, step, stepT, maxCurr, newSweep):
-    rm = visa.ResourceManager()
-    B2901A_address = rm.list_resources()[0]
-    MM_34460_address = rm.list_resources()[1]
-    smu = rm.open_resource(B2901A_address)
-    mm = rm.open_resource(MM_34460_address)
-    aperture = stepT
-    nplcTime = 10
-    if newSweep == 1:
-        smu.write('*RST')
-        mm.write('*RST')
-        smu.write(':SOUR:FUNC:MODE VOLT')
-        smu.write(':SOUR:VOLT 0')
-        smu.write(':SOUR:VOLT:RANG 20')
-        smu.write(':SOUR:VOLT {}'.format(Vbegin))
-        smu.write(':SENS:FUNC ""CURR""')
-        smu.write(':SENS:CURR:RANG:AUTO ON')
-        smu.write(':SENS:CURR:PROT {}'.format(maxCurr))
-        smu.write(':SENS:CURR:APER {}'.format(aperture))
-        smu.write(':FORM:DATA ASC')
-        smu.write(':OUTP ON')
-        mm.write(':CONF:CURR:DC')
-        mm.write(':CURR:DC:NPLC {}'.format(nplcTime))
-    countList = []
-    countTemp = Vbegin
-    if (Vend>Vbegin):
-        for i in range(int(1+(Vend-Vbegin)/step)):
-            countList.append(countTemp)
-            countTemp = countTemp + step
-    if (Vbegin>Vend):
-        countTemp = Vend
-        for i in range(int(1+(Vbegin-Vend)/step)):
-            countList.append(countTemp)
-            countTemp = countTemp + step
-        countList = list(reversed(countList))
+    B2901A = rm.list_resources()[0]
+    K34460A = rm.list_resources()[1]
+    smu = rm.open_resource(B2901A)
+    mm = rm.open_resource(K34460A)
+    smu.write("*RST")
+    print("SMU: {}".format(smu.query("*IDN?")))
+    smu.write(":SOUR:FUNC:MODE CURR")
+    smu.write(":SOUR:CURR:RANG 0.01")
+    smu.write(":SOUR:CURR {}".format(sourceCurrent))
+    smu.write(":SENS:FUNC 'VOLTS'")
+    smu.write(":SENS:VOLT:RANG 200")
+    smu.write(":SENS:VOLT:NPLC 1")
+    smu.write(":SENS:VOLT:PROT 21")
+    mm.write("*RST")
+    mm.write(":CONF:CURR:DC")
+    mm.write(":CURR:DC:NPLC 1")
+    print("{} sec left".format(maxTime))
+    tbegin = datetime.datetime.now()
+    smu.write(":OUTP ON")
     voltList = []
     currList = []
+    timeList = []
     brightList = []
-    for i in countList:
-        smu.write(':SOUR:VOLT {}'.format(i))
-        currMeasurement = smu.query(':MEAS:CURR? (@1)').split('\n')[0]
-        voltMeasurement = smu.query(':MEAS:VOLT? (@1)').split('\n')[0]
-        #brightMeasurement = mm.query(':MEAS:CURR:DC?').split('\n')[0]
-        brightMeasurement = mm.query(':READ?').split('\n')[0]
-        voltList.append(voltMeasurement)
-        currList.append(currMeasurement)
-        brightList.append(brightMeasurement)
-    VIBList = list(zip(voltList,currList,brightList))
-    return VIBList
+    smu.write(":INIT")
+    tRun = datetime.datetime.now() - tbegin
+    twait = 0
+    ret = ""
+
+    while (tRun.seconds<maxTime):
+        voltList.append(smu.query(":MEAS:VOLT?").split('\n')[0])
+        currList.append(smu.query(":MEAS:CURR?").split('\n')[0])
+        brightTemp = mm.query(":MEAS:CURR?").split('\n')[0]
+        brightList.append(brightTemp)
+        if float(brightTemp) < .005e-6:
+            print("No Output! Breaking Decay Loop...")
+            ret = "fail"
+            break
+        tRun = datetime.datetime.now() - tbegin
+        timeList.append(tRun.seconds+(1E-6)*tRun.microseconds)
+        time.sleep(twait)
+    smu.write(":OUTP OFF")
+    outList = list(zip(timeList,voltList,currList,brightList))
+    smu.write(":OUTP OFF")
+    if ret != "fail":
+        return outList
+    if ret == "fail":
+        return "fail"
+
+def IVBSweep(Vbegin, Vend, step, maxCurr, newSweep):
+    rm = visa.ResourceManager()
+    B2901A_address = rm.list_resources()[0]
+    MM_34460_address = rm.list_resources()[1]
+    smu = rm.open_resource(B2901A_address)
+    mm = rm.open_resource(MM_34460_address)
+    mm.timeout = 5000000
+    smu.timeout = 5000000
+    aperture = .15
+    nplcTime = 1
+    if newSweep == 1:
+        smu.write(":SOUR:FUNC:MODE VOLT")
+        smu.write(":SOUR:VOLT:MODE SWE")
+        smu.write(":SOUR:VOLT:RANG 20")
+        smu.write(":SOUR:VOLT:STAR {}".format(Vbegin))
+        smu.write(":SOUR:VOLT:STOP {}".format(Vend))
+        smu.write(":SOUR:VOLT:POIN {}".format(step))
+        smu.write(":SOUR:SWE:DIR UP")
+        smu.write(":SOUR:SWE:SPAC LOG")
+
+        mm.write('*RST')
+        mm.write(':CONF:CURR:DC')
+        mm.write(':CURR:DC:NPLC {}'.format(nplcTime))
+        mm.write(':TRIG:COUN {}'.format(step))
+        mm.write(':TRIG:SOUR EXT')
+        mm.write(':TRIG:DEL .01')
+        mm.write(':INIT')
+
+        smu.write(':SENS:FUNC ""CURR""')
+        smu.write(':SENS:CURR:RANG:AUTO ON')
+        smu.write(':SENS:CURR:APER {}'.format(aperture))
+        smu.write(':SENS:CURR:PROT {}'.format(maxCurr))
+        smu.write(':FORM:DATA ASC')
+        smu.write(':TRIG:SOUR AINT')
+        smu.write(':TRIG:COUN {}'.format(step))
+        smu.write(":TRIG:DEL .01")
+        smu.write(':OUTP ON')
+        smu.write(":SOUR:TOUT:STAT ON")
+        smu.write(":SOUR:TOUT:SIGN EXT3")
+        smu.write(":SOUR:DIG:EXT3:FUNC DIO")
+        smu.write(":SOUR:DIG:EXT3:TOUT:EDGE:POS BEF")
+        smu.write(':INIT (@1)')
+
+        brightCurr = mm.query(":FETC?").split(',')
+        measCurr = smu.query(':FETC:ARR:CURR? (@1)').split(',')
+        sourceVolts = smu.query(':FETC:ARR:VOLT? (@1)').split(',')
+
+        smu.write(':OUTP OFF')
+        VIBList = list(zip(sourceVolts,measCurr,brightCurr))
+        return VIBList
 
 # Executes a linear IV sweep and returns IV output
 def LinSweepSMU(Vbegin, Vend, samplePoints, stepT, maxCurr):
@@ -86,6 +116,7 @@ def LinSweepSMU(Vbegin, Vend, samplePoints, stepT, maxCurr):
     smu = rm.open_resource(B2901A_address)
     totalTime = (stepT+.005)*samplePoints
     smu.write('*RST')
+    print("SMU: {}".format(smu.query("*IDN?")))
     smu.write(':SOUR:FUNC:MODE VOLT')
     smu.write(':SOUR:VOLT:MODE SWE')
     smu.write(':SOUR:VOLT:RANG 20')
@@ -114,10 +145,12 @@ def LinSweepSMU(Vbegin, Vend, samplePoints, stepT, maxCurr):
 def SMUclose():
     rm = visa.ResourceManager()
     B2901A_address = rm.list_resources()[0]
+    MM_34460_address = rm.list_resources()[1]
+    mm = rm.open_resource(MM_34460_address)
     smu = rm.open_resource(B2901A_address)
     smu.write(':OUTP OFF')
     smu.close()
-
+    mm.close()
 
 # Returns a string of the current time: "YYYYMMDD-HH-mm-ss"
 def stringTime():
@@ -151,6 +184,15 @@ def writeIVB(fn,data):
     with open(fn, 'w',newline='') as csvfile:
         writer = csv.writer(csvfile)
         row1 = ["Voltage (V)","Current (A)","Brightness Current (A)"]
+        writer.writerow(row1)
+        for row in data:
+            writer.writerow(row)
+
+# Outputs IV-Brightness Decay data to a .csv
+def writeIVBDecay(fn,data):
+    with open(fn, 'w',newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        row1 = ["Time (sec)","Voltage (V)","Current (A)","Brightness Current (A)"]
         writer.writerow(row1)
         for row in data:
             writer.writerow(row)
--- scratch.py
+++ scratch.py
@@ -1,10 +1,31 @@
-Vstart = 1
-Vend = 10
-step = .5
-countList = []
-countTemp = Vstart
-for i in range(int(1+(Vend-Vstart)/step)):
-    countList.append(countTemp)
-    countTemp = countTemp + step
-print(countList)
+import OLEDTools
+import time
+import serial
+import visa
+import datetime
 
+# mux = [1,2,3,4,5,6,7,8]
+# ser = serial.Serial('COM3', 9600, timeout=0)
+# for i in mux:
+#     time.sleep(.5)
+#     ser.write(b'+0')
+#     time.sleep(2)
+#     ser.write(str.encode('-' + str(i)))
+#     print(i)
+#     time.sleep(15)
+#
+#     OLEDTools.currDecay(-5e-3,5)
+# ser = serial.Serial('COM3',9600,timeout=0)
+# time.sleep(0.5)
+# ser.write(b'+0')
+# time.sleep(2)
+# ser.write(b'-1')
+# time.sleep(2)
+#
+# VIBList = OLEDTools.IVBSweep(5,0,51,0.02,1)
+# OLEDTools.writeIVB("foo3.csv",VIBList)
+#ser.write(b'+1')
+
+xOut = [x*1E-2 for x in range(0,150)]
+
+print(xOut)
